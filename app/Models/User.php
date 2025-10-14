e5e75d739fc70ae69bf99a54328c1e119f2d6005
Smorareq1 <smorareq1@gmail.com>
2025-10-13 21:20:50 -0600

    refactor(user): clean up comments and improve code readability in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,14 +11,7 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'username',
         'first_name',
@@ -29,21 +22,11 @@ class User extends Authenticatable
     ];
 
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
