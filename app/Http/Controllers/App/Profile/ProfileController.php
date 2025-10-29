06d172555ed8770d29f447101b0f0b6f456980ab
Christopher <chrisyumanv@gmail.com>
2025-10-29 13:19:06 -0600

    Arreglar actualizar y notificacion

--- app/Http/Controllers/App/Profile/ProfileController.php
+++ app/Http/Controllers/App/Profile/ProfileController.php
@@ -6,18 +6,18 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Validation\Rule;
 use Illuminate\Validation\Rules\Password;
-use Illuminate\Support\Facades\Log;
 
 class ProfileController extends Controller
 {
     public function update(Request $request)
     {
-        // Si la petición es de API, usa el guard 'api'. Si no, usa el 'web'.
-        $user = auth($request->expectsJson() ? 'api' : 'web')->user();
+        // La ruta está protegida por el middleware 'auth', por lo que podemos obtener al usuario de la solicitud.
+        $user = $request->user();
 
-        $validatedData = $request->validate([
+        $request->validate([
             'first_name' => ['required', 'string', 'min:2', 'max:50'],
             'last_name' => ['required', 'string', 'min:2', 'max:50'],
             'username' => ['required', 'string', 'min:3', 'max:50', 'regex:/^[a-zA-Z0-9_\s]+$/', Rule::unique('users')->ignore($user->id)],
@@ -33,21 +33,20 @@ class ProfileController extends Controller
         }
 
         if ($request->hasFile('photo')) {
+            // Eliminar el avatar antiguo si existe
+            if ($user->avatar) {
+                Storage::disk('public')->delete($user->avatar);
+            }
             $path = $request->file('photo')->store('avatars', 'public');
             $updateData['avatar'] = $path;
         }
 
         $user->update($updateData);
 
-
-        if ($request->expectsJson()) {
-            return response()->json([
-                'message' => '¡Perfil actualizado con éxito!',
-                'user' => $user->fresh() // Devuelve el usuario actualizado
-            ], 200);
-        }
-
-        // Si es una petición web (Inertia), haz la redirección
-        return redirect()->back()->with('success', '¡Perfil actualizado con éxito!');
+        // La ruta de la API siempre espera una respuesta JSON.
+        return response()->json([
+            'message' => '¡Perfil actualizado con éxito!',
+            'user' => $user->fresh() // Devuelve el usuario actualizado
+        ], 200);
     }
 }
