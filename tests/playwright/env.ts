56500d38a04e2154f6daa8d905dcf5a67aab1849
Smorareq1 <smorareq1@gmail.com>
2025-11-04 20:12:20 -0600

    Add AdminUserSeeder and update test configurations; fix last run status

---
+++ tests/playwright/env.ts
@@ -0,0 +1,11 @@
+export const testConfig = {
+    TestLogin: {
+        Email: 'basic_user@gmail.com',
+        Password: 'SomePassword123'
+    },
+    TestRecommendation: {
+        Email: 'admin@gmail.com',
+        Password: 'Admin@1234',
+        ImageUrl: 'https://amzn-s3-anima.s3.us-east-2.amazonaws.com/tests/PersonaTriste.jpg'
+    },
+};
